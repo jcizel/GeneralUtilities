2fcbe954d1a1c6f7769d38d5ec9ac44f6db982c8
jcizel <jcizel@gmail.com>
2014-11-19 19:45:31 +0100

    add shift function

---
+++ R/shift.R
@@ -0,0 +1,47 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Lag a time series within a panel 
+##' @param x numeric vector to be shifted
+##' @param shift.by number of legs
+##' @param dif 
+##' @param relative 
+##' @return a vector 
+##' @author Janko Cizel
+shift<-function(x,
+                lag = -1,
+                dif = FALSE,
+                relative = FALSE){
+    stopifnot(is.numeric(lag))
+    ## stopifnot(is.numeric(x))
+
+    ## if (length(lag)>1)
+    ##     return(lapply(lag,shift, x=x))
+
+    out <- NULL
+    abs.lag=abs(lag)
+
+    if (length(x)<abs.lag){
+        warning("Number of lags exceeds the length of the vector.")
+        out <- rep(NA,length(x))
+    } else {
+        if (lag > 0 )
+            out <- c(tail(x,-abs.lag),
+                     rep(NA,abs.lag))
+        else if (lag < 0 )
+            out <- c(rep(NA,abs.lag),
+                     head(x,-abs.lag))
+    }
+    
+    if (dif == TRUE){
+        if (relative ==TRUE){
+            out <- (x - out)/out
+        } else {
+            out <- x - out            
+        }
+    }
+
+    out[is.infinite(out)] <- NA
+    
+    return(out)
+}
