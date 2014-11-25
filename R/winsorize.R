db550a7b7b934676aedb6771a33c9c9a387ed54e
jcizel <jcizel@gmail.com>
2014-11-25 13:59:43 +0100

    improvements in winsorization function

--- R/winsorize.R
+++ R/winsorize.R
@@ -1,22 +1,51 @@
-winsorize <- function(x,
-                      method = "IQR",
-                      k=2,
-                      fraction = 0.01,
-                      na.rm = TRUE, ...) {
-    if (is.numeric(x) & method == "IQR"){
-        qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
-        H <- k * IQR(x, na.rm = na.rm)
-        x[x < (qnt[1] - H)] <- (qnt[1] - H)
-        x[x > (qnt[2] + H)] <- (qnt[2] + H)
-        x
-    } else if (is.numeric(x) & method == "PERC"){
-        lim <- quantile(x, probs=c(fraction, 1-fraction), na.rm = na.rm)
-        x[ x < lim[1] ] <- lim[1]
-        x[ x > lim[2] ] <- lim[2]
-        x
-    }
-    else{
-        warning("The input is either not numeric or you supplied a faulty method of winsorization")
-        as.factor(x)
+##' This function winsorizes an input numeric vector
+##'
+##' .. TODO ..
+##' @title Winsorize a vector
+##' @param x a numeric vector
+##' @param method method of winsorization. 
+##' @param k only applicable if method=="IQR". Specifies a number of
+##' interquartile ranges from each quartile that define the winsorization limits.
+##' @param fraction only applicable if method=="PERC"
+##' @param na.rm omit missing values? Default is TRUE.
+##' @param trim Trim extreme values rather setting them to a limit values?
+##' Default is FALSE
+##' @return winsorized numeric vector
+##' @author Janko Cizel
+winsorize <- function(
+    x,
+    method = c("IQR","PERC"),
+    k=2,
+    fraction = 0.01,
+    na.rm = TRUE,
+    trim = FALSE)
+{
+    if (is.numeric(x)){
+        if (method == "IQR"){
+            qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+            H <- k * IQR(x, na.rm = na.rm)
+            if (trim == FALSE){
+                x[x < (qnt[1] - H)] <- (qnt[1] - H)
+                x[x > (qnt[2] + H)] <- (qnt[2] + H)
+            } else {
+                x[x < (qnt[1] - H)] <- NA
+                x[x > (qnt[2] + H)] <- NA
+            }
+            return(x)
+        } else if (method == "PERC"){
+            lim <- quantile(x, probs=c(fraction, 1-fraction), na.rm = na.rm)
+            if (trim == FALSE){
+                x[ x < lim[1] ] <- lim[1]
+                x[ x > lim[2] ] <- lim[2]
+            } else {
+                x[ x < lim[1] ] <- lim[1]
+                x[ x > lim[2] ] <- lim[2]
+            }
+            return(x)
+        } else {
+            stop("Specify a winsorization method!")
+        }
+    } else {
+        stop("x must be a numeric vector!")
     }
 }
