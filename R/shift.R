a66a74ca2effdee7f585d1a83a6853d91ca419f3
jcizel <jcizel@gmail.com>
2014-11-26 21:30:00 +0100

    added docs

--- R/shift.R
+++ R/shift.R
@@ -46,24 +46,46 @@ shift<-function(x,
     return(out)
 }
 
-
+##' shiftData implements lead/lag/dif operations on a data.table and returns a
+##' resulting dataset.
+##'
+##' .. content for \details{} ..
+##' @title Lead/lag/dif operations on a data.table
+##' @param data input dataset. Must be a data.table. 
+##' @param var character vector with names of variables that should be modified
+##' @param replace replace the original variables. If FALSE (by default), the
+##' modified variables are added to the original dataset, with a suffix added
+##' to the name of the original variables.
+##' @param by character vector with name(s) of by groups.
+##' @param lag number of lags (negative) or leads (positive). 
+##' @param dif take a difference? Default is FALSE.
+##' @param relative  relative differences? Applicable only if dif=TRUE.
+##' @return data.table with the resulting dataset.
+##' @author Janko Cizel
 shiftData <- function(
     data,
     var,
+    replace = FALSE,
     by = NULL,
     lag = -1,
     dif = FALSE,
     relative = FALSE
 ){
+    if (!inherits(data, 'data.table')) stop('Data must be a data.table.')
+    if (dif == FALSE) relative <- FALSE
+    
     dt <- copy(data)
     
     for (x in var){
-        .n <- paste0(x,
-                     ".",
-                     ifelse(sign(lag)<0,'L','F'),
-                     abs(lag),
-                     ifelse(dif == TRUE, '.DIF',''),
-                     ifelse(relative == TRUE, '.REL',''))
+        if (replace == FALSE){
+            .n <- paste0(x,
+                         ".",
+                         ifelse(sign(lag)<0,'L','F'),
+                         abs(lag),
+                         ifelse(dif == TRUE, '.DIF',''),
+                         ifelse(relative == TRUE, '.REL',''))
+        } else .n <- x
+        
         if (!is.null(by)){            
             dt[, (.n) := {
                 .x <- get(x)
@@ -86,3 +108,13 @@ shiftData <- function(
 
     return(dt)
 }
+
+## shiftData(
+##     data = data.table(iris),
+##     var = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),
+##     replace = FALSE,
+##     by = 'Species',
+##     lag = -5,
+##     dif = FALSE,
+##     relative = FALSE
+## )
