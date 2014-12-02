b1c20af3a8a839014821ddfc72fa807208a82307
jcizel <jcizel@gmail.com>
2014-12-02 20:50:08 +0100

    add date converter

---
+++ R/dates.R
@@ -0,0 +1,26 @@
+convertToDate <- function(x){
+    require(lubridate)
+
+    x <- as.factor(x)
+    levels <- levels(x)
+    
+    .converter <- function(.x){
+        if (.x %like% "[0-9]+Q[0-9]+"){
+            o <- .x %>>%
+            stringr:::str_split('Q') %>>%
+            (l ~ l[[1]])
+
+            year <- o[[1]] %>>% as.numeric
+            qrt <- o[[2]] %>>% as.numeric 
+
+            date <- as.character(as.Date(paste0(year,'-',qrt*3,'-',1)) + months(1) - days(1))
+        } else if (.x %like% "^[0-9]{4}$") {
+            date <- paste0(.x, "-12-31")
+        }
+
+        return(date)
+    }
+
+    levelsm <- Vectorize(.converter)(levels) %>>% as.Date
+    return(levelsm[x])
+}
