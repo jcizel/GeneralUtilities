ff4775e8c9bb98fa0cdd4159e756743858e0578f
Janko Cizel <jankocizel@Jankos-MacBook-Pro-2.local>
2014-12-15 20:06:43 +0100

    change in conversion syntax

--- R/gems.R
+++ R/gems.R
@@ -49,76 +49,136 @@ procExpand <- function(
     keepvars = NULL,
     dropvars = NULL,
     convert =
-        list('_NUMERIC_ ~ `+`(1);`-`(2)',
-             '_CHAR_ ~ nchar'),
+        list('~ _NUMERIC_ ~ `+`(1);`-`(2)',
+             '~ _CHAR_ ~ nchar'),
     prefix = '',
     suffix = ''
 ){
-    if (!inherits(data,'data.table')) stop('`data` must be a data.table!')
-    ## if (by == '' || (keepvars %||% 'ok' == '')) stop("`by` and `keepvars` cannot be empty strings!")
-    if (!is.null(keepvars) && !is.null(dropvars)) stop('`keepvars` and `dropvars` cannot both be specified simultaneously!')
+    if (!inherits(data,'data.table'))
+        stop('`data` must be a data.table!')
+    if (!is.null(keepvars) && !is.null(dropvars))
+        stop('`keepvars` and `dropvars` cannot both be specified simultaneously!')
 
-    .convert <- .parseConvert(convert)
+    ## .convert <- .parseConvert(convert)
+    .convert <- .parseConvert2(convert)
 
     if (is.null(by)){
         .convert %>>%
         list.update(vars = .varList(data,vars,drop = dropvars)) %>>%
+        list.update(vars,labs = trim(stringr::str_split(labs,",")[[1]]) %>>% .parseLabels) %>>% 
         list.update(vars,vars = trim(stringr::str_split(vars,",")[[1]])) %>>%
         list.update(oper,oper = trim(stringr::str_split(oper,";")[[1]])) %>>%        
         (~ .c)
-    }
-    else{
-        .convert %>>% list.update(vars =
-                                      .varList(data,
-                                               vars,
-                                               drop = if (is.null(dropvars)) by else c(dropvars,by))) %>>%
+    } else {
+        .convert %>>%
+        list.update(vars = .varList(data,vars,drop = if (is.null(dropvars)) by else c(dropvars,by))) %>>%
         list.update(vars = .varList(data,vars,drop = dropvars)) %>>%
+        list.update(vars,labs = trim(stringr::str_split(labs,",")[[1]]) %>>% .parseLabels) %>>%      
         list.update(vars,vars = stringr::str_split(vars,",")[[1]]) %>>%
         list.update(oper,oper = trim(stringr::str_split(oper,";")[[1]])) %>>%                
         (~ .c)
     }
 
-    .c %>>%
-    list.map(. %>>%  list.extract("vars")) %>>%
-    unlist %>>%
-    unique %>>%
-    (x ~ x[x!=""]) %>>%
-    (~ vars)    
-    
-    if (is.null(by)){
-        keep <- c(keepvars,vars) %||% vars
-        keep <- keep[keep != ""]
-        dt <- copy(data[,.SD,.SDcols = c(keep)])
-        for (x in vars){
-            o <- rlist::list.find(.c, x %in% vars)[[1]][['oper']]
-            ## dt[,(x) := modify(x = get(x), operations = o)]
-            dt[,paste0(prefix,x,suffix) := modify(x = get(x), operations = o)]            
-            setcolorder(dt, orderElements(names(dt),c(keepvars) %||% names(dt),'first'))
+    out <- 
+        foreach(l = 1:length(.c)) %do% {
+            .l <- .c[[l]]
+
+            if (.l$vars == "")
+                return(NULL)
+            
+            if (is.null(by)){
+                keep <-  unique(.l$vars)
+                
+                dt <- copy(data[,.SD,.SDcols = c(keep)])
+                for (x in .l$vars){
+                    dt[,paste0(x) :=
+                           modify(x = get(x),
+                                  operations = .l$oper)]
+                    setnames(dt,
+                             x,
+                             paste0(.l$labs$prefix,
+                                    x,
+                                    .l$labs$suffix))                    
+                }
+                return(dt)
+                
+            } else {
+                keep <- unique(c(.l$vars,by))
+                dt <- copy(data[,.SD,.SDcols = c(keep)])
+                for (x in .l$vars){
+                    dt[,paste0(x) :=
+                           modify(x = get(x),
+                                  operations = .l$oper),
+                       by = by]
+                    
+                    setnames(dt,
+                             x,
+                             paste0(.l$labs$prefix,
+                                    x,
+                                    .l$labs$suffix))
+                }
+
+                dt[,paste0(by) := NULL]
+
+                return(dt)
+            }        
         }
-    } else {
-        keep <- c(keepvars,vars,by) %||% c(vars,by)
-        keep <- keep[keep != ""]
-        dt <- copy(data[,.SD,.SDcols = c(keep)])
-        for (x in vars){
-            o <- rlist::list.find(.c, x %in% vars)[[1]][['oper']]
-            ## dt[,(x) := modify(x = get(x), operations = o)
-            ##    , by = by]
-            dt[,paste0(prefix,x,suffix) := modify(x = get(x), operations = o)
-               , by = by]            
-            setcolorder(dt, orderElements(names(dt),c(by,keepvars) %||% c(by),'first'))
-        }        
-    }
-    return(dt)
+ 
+    out2 <- Filter(function(x) !is.null(x), out)
+
+    names(out2) <- NULL
+    result <- do.call('cbind', out2)
+
+    if (!is.null(by))
+        result <-
+            cbind(
+                copy(data[,.SD,.SDcols = c(by)]),
+                result
+            )
+    
+    if (!is.null(keepvars))
+        result <-
+            cbind(
+                copy(data[,.SD,.SDcols = c(keepvars)]),
+                result
+            )
+   
+    return(result)
 }
 
 ## mtcars %>>% data.table %>>% (~ dt)
 ## by = 'gear'
 
-## ## keepvars = c('hp','vs')
-## ## convert <-
-## ##     list('drat,wt'=c('shift(lag=-1,dif = TRUE,relative = TRUE)','`-`(1)'),
-## ##          'qsec'=c('`+`(5)','`/`(10)'))
+## keepvars = c('hp','vs')
+## convert <-
+##     list('pre:dif.~ drat,wt ~ shift(lag=-1,dif = TRUE,relative = FALSE)',
+##          'pre:dif.,suf:.rel ~ drat,wt ~ shift(lag=-1,dif = TRUE,relative = TRUE);`*`(100)')
 
+## procExpand(
+##     data = dt,
+##     by = 'gear',
+##     keepvars = c('mpg','drat','wt'),
+##     convert = convert    
+## )
+
+.parseLabels <- function(labs = c("pre:test", "suf:test2")){
+   .p <- grep("pre:",labs,value = TRUE)
+   if (length(.p) == 1){
+       prefix = gsub("(pre:)(.+)",
+           "\\2",
+           .p)
+   } else prefix = ""
+
+   .s <- grep("suf:",labs,value = TRUE)
+   if (length(.p) == 1){
+       suffix = gsub("(suf:)(.+)",
+           "\\2",
+           .s)
+   } else suffix = ""
+
+   return(list(prefix = prefix,
+               suffix = suffix))
+}
 
 ## by = 'gear'
 ## keepvars = 'cyl'
@@ -157,6 +217,30 @@ procExpand <- function(
             list(vars = vars,
                  oper = oper)
         }
+    return(o)
+}
+
+
+.parseConvert2 <- function(convert =
+                               list('pre:, suf:~ var1,var2 ~ `+`1',
+                                    'pre:, suf:~ var3,var4 ~ `-`1')){
+    p <- lapply(convert,function(x){trim(stringr::str_split(x,'~')[[1]])})
+
+    convert %>>%
+    list.map(. ~ trim(stringr::str_split(.,'~')[[1]])) %>>%
+    (~ p)
+    
+    o <- 
+        foreach (x = 1:length(convert)) %do% {
+            ## vars = stringr::str_split(string = p[[x]][[1]],',')[[1]]
+            labs = p[[x]][[1]]            
+            vars = p[[x]][[2]]            
+            oper = p[[x]][[3]]
+            list(labs = labs,
+                 vars = vars,
+                 oper = oper)
+        }
+    
     return(o)
 }
 
